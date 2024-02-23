deb7fcc3068f44c995ed7983997d43deec13e594
rajdev12 <rjnath98@gmail.com>
2024-02-23 11:28:58 +0530

    initial commit

--- src/GUI/Table_Demo.java
+++ src/GUI/Table_Demo.java
@@ -1,7 +1,29 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Table_Demo {
     public static void main(String[] args) {
+        JFrame jf=new JFrame("Table Demo");
+        jf.setSize(800,600);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String data[][]={
+                {"100","Lakshay", "22"},
+                {"101","Puneeth","23"},
+                {"102","Rahul","24"},
+                {"103","Rahul","24"},
+                {"104","Raj","24"}
+        };
+        String []col={"ROll No","Name","Age"};
+
+        JTable jt =new JTable(data,col);
+        jf.add(new JScrollPane(jt));
+        jf.setFont(new Font("Arial",Font.BOLD,45));
+        jt.setRowHeight(50);
+        jf.setVisible(true);
 
     }
 }
