e59f671f1c375f0b08dc4d277acf7f31a3cd550d
rajdev12 <rjnath98@gmail.com>
2024-02-23 11:14:24 +0530

    sdding the table demo in swing

---
+++ src/GUI/Table_Demo.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class Table_Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/LL/ll.java
@@ -0,0 +1,22 @@
+package LL;
+
+public class ll {
+    private Node head;
+    private Node tail;
+    Private int next;
+
+
+    class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value=value;
+            this.next=next;
+        }
+        Node(int value,Node next){
+            this.value=value;
+            this.next=next;
+    }
+
+}
+        }
